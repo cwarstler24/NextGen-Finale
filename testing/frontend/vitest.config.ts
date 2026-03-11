531040442ce634fbf50506438cbd09e2cea66dd4
Clayton Warstler - Rocket Software <cwarstler@rocketsoftware.com>
2026-03-11 12:08:38 -0500

    style: update ESLint rules for Vue and reformat vitest.config.ts

--- testing/frontend/vitest.config.ts
+++ testing/frontend/vitest.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    include: ['testing/frontend/tests/**/*.test.{ts,js,tsx,jsx}'],
-    globals: true,
-    reporters: ['default'],
-    coverage: {
-      enabled: false
-    }
-  },
+    test: {
+        environment: 'jsdom',
+        include: ['testing/frontend/tests/**/*.test.{ts,js,tsx,jsx}'],
+        globals: true,
+        reporters: ['default'],
+        coverage: {
+            enabled: false
+        }
+    },
 });
