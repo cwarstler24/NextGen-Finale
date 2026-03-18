c9691118f0f823d34e34889364cd3d83817bb143
Travis <tclark2@rocketsoftware.com>
2026-03-18 15:42:56 -0500

    Added framework for burger menu

---
+++ main/frontend/src/views/BurgerMenuView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useRouter } from 'vue-router'
+import customBurger from '../data/customBurgers.js';
+const router = useRouter()
+
+const goToBurgerPage = () => {
+    router.push({ name: 'product' , params: { product: 'Burger' }})
+}
+</script>
+
+<template>
+<div class="page">
+    <div class="card" @click="goToBurgerPage">
+        <h2>Custom Burger Menu</h2>
+        <p>Explore our selection of custom burgers, crafted with the finest ingredients and tailored to your taste. Click here to view our menu and start building your perfect burger today!</p>
+    </div>
+    <!-- loops through all items in the JSON object-->
+    <div v-for="burger in customBurger" :key="burger.name" class="card">
+        <h2>{{ burger.name }}</h2>
+        <p>{{ burger.description }}</p>
+    </div>
+</div>
+</template>
+
+<style src="../styles/BurgerMenuView.css"></style>
